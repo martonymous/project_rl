c958b99803b72ae4439b36bba4e1f8bfeba67fb3
Marton <marton@wearebit.com>
2023-01-17 22:20:12 +0100

    updated evaluation (include Antonio's changes), modified epsilon

--- environment.py
+++ environment.py
@@ -19,9 +19,11 @@ class DamWorldEnv(gym.Env):
         self.flow_multiplier = {
             0: 1,
             1: 2,
-            2: 4
+            2: 4,
+            3: 9,
+            4: 18
         }
-        self.flow_rate = 5000
+        self.flow_rate = 1000
 
         self.sell_efficiency = 0.9
         self.buy_efficiency = 0.8
@@ -33,6 +35,7 @@ class DamWorldEnv(gym.Env):
         self.water_level_dim = 21
         self.water_capacity = self.flow_rate * self.water_level_dim
         # for converting water level (m³) to MWh
+        #  pot. energy of 1 m³ = mass *    g *  h * (Joule to MWH factor)
         self.conversion_factor = 1000 * 9.81 * 30 * (2 + (7/9)) * (10 ** -10)
 
         self.action_space = spaces.MultiDiscrete([len(self.Actions), len(self.flow_multiplier)])
@@ -68,9 +71,9 @@ class DamWorldEnv(gym.Env):
     
     def _get_info(self):
         return {
-            "profit": (self.cash - self.starting_cash) * self.conversion_factor,
+            "profit": (self.cash - self.starting_cash),
             "unrealized_profit": self.water_level * self.electricity_cost * self.sell_efficiency * self.conversion_factor,
-            "total_value": (self.cash - self.starting_cash) + (self.water_level * self.electricity_cost * self.sell_efficiency) * self.conversion_factor
+            "total_value": (self.cash - self.starting_cash) + (self.water_level * self.electricity_cost * self.sell_efficiency * self.conversion_factor)
         }
     
     def step(self, action, terminated=False):
@@ -89,7 +92,7 @@ class DamWorldEnv(gym.Env):
             flow_mult = self.flow_multiplier[action[1]]
 
             # we can only sell if there is water in the dam
-            if action[0] == 0 and self.water_level != 0:
+            if (action[0] == 0) and (self.water_level != 0):
                 if self.water_level > (flow_mult * self.flow_rate):
                     self.cash += self.electricity_cost * self.sell_efficiency * flow_mult * self.flow_rate * self.conversion_factor
                     self.water_level -= flow_mult * self.flow_rate
@@ -98,7 +101,7 @@ class DamWorldEnv(gym.Env):
                     self.water_level = 0
                     
             # we can only buy if we have cash and if dam is not full
-            elif action[0] == 1 and self.water_level < self.water_capacity and self.cash > (self.electricity_cost * self.sell_efficiency * (self.water_capacity - self.water_level) * self.conversion_factor):
+            elif (action[0] == 1) and (self.water_level < self.water_capacity) and (self.cash > (self.electricity_cost * self.sell_efficiency * (self.water_capacity - self.water_level) * self.conversion_factor)):
                 if (self.water_capacity - self.water_level) > (flow_mult * self.flow_rate):
                     self.cash -= self.electricity_cost * self.buy_efficiency * flow_mult * self.flow_rate * self.conversion_factor
                     self.water_level += flow_mult * self.flow_rate
@@ -109,12 +112,20 @@ class DamWorldEnv(gym.Env):
         observation = self._get_obs()
         info = self._get_info()
         self.value = info["total_value"]
-        reward = self.value - previous_total_value
+        
+        if self.value > previous_total_value:
+            reward = 1
+        else:
+            reward = -1
 
         return observation, reward, terminated, False, info
 
-    def reset(self):
-        self.index = random.randint(0, len(self.data) - 50)
+    def reset(self, val=False):
+        if val:
+            self.index = 0
+        else:
+            self.index = random.randint(0, len(self.data) - 50)
+
         self.hour = self.data["hour"].iloc[self.index]
         self.day = self.data["day"].iloc[self.index]
         self.week = self.data["week"].iloc[self.index]
@@ -124,7 +135,7 @@ class DamWorldEnv(gym.Env):
         self.starting_cash = 0  # (arbitrary) amount of cash
         self.cash = self.starting_cash
         self.electricity_cost = self.data["prices"].iloc[self.index]
-        self.value = self.starting_cash + (self.electricity_cost * self.water_level)
+        self.value = self.starting_cash + (self.electricity_cost * self.water_level * self.conversion_factor)
         
         observation = self._get_obs()
         info = self._get_info()
--- visualize.py
+++ visualize.py
@@ -3,14 +3,11 @@ import pandas as pd
 import numpy as np
 
 
-def merge_dfs(val: pd.DataFrame, choices: pd.DataFrame):
-    return pd.concat([val.reset_index(drop=True), choices.reset_index(drop=True)], axis=1)
-
 def visualize_validation(filename: str, start: int, end: int):
     val = pd.read_csv(filename)
 
     ys = val["prices"].to_list()[start:end]
-    choices = val["choices"].to_list()[start:end]
+    choices = val["actions"].to_list()[start:end]
 
     sell_markers = [i for i, x in enumerate(choices) if x == 0]
     buy_markers = [i for i, x in enumerate(choices) if x == 1]
@@ -19,3 +16,6 @@ def visualize_validation(filename: str, start: int, end: int):
     plt.plot(list(range(len(ys))), ys, color="black", marker="o", mfc="red", ls="dotted", markersize=10, markevery=buy_markers, label="buy electricity")
     plt.legend()
     plt.show()
+
+if __name__ == "__main__":
+    visualize_validation("data/eval.csv", 1000, 1250)
